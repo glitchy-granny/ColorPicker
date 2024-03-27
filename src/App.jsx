aa4f259bc04ce5060aa74adac0f7f93b63c058fa
Pamela Daniels <pamela.d.daniels@gmail.com>
2024-03-27 17:36:11 -0400

    updated selectedColor to Currently selected div

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 // Write your Color component here
+import { useState } from "react";
+
+const Color = ({ color, setFunction }) => {
+  return <div className={color} onClick={() => setFunction(color)}></div>;
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("blue");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+
+      <div id="colors-list">
+        {
+          <>
+            <Color color="blue" setFunction={setSelectedColor} />
+            <Color color="violet" setFunction={setSelectedColor} />
+            <Color color="green" setFunction={setSelectedColor} />
+          </>
+        }
+      </div>
     </div>
   );
 };
